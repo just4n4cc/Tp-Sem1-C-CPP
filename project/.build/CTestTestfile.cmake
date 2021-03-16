23db5d279215993f144c25713913da794fdf9a3e
just4n4cc <just4n4cc@yandex.ru>
2021-03-16 19:39:52 +0300

    go go go go

--- project/.build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/tp/Sem1/Tp-Sem1-C-CPP/project
-# Build directory: /home/user/tp/Sem1/Tp-Sem1-C-CPP/project/.build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
